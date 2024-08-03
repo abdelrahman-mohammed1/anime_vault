bc0a4d5a63033e83baa3d4e7093d30455c4135a1
abdelrahman-mohammed1 <UGS.141356@ci.suez.edu.eg>
2024-08-03 23:16:10 +0300

    upload anime project files

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,9 +1,12 @@
 import Image from "next/image";
 
 function Footer() {
+  const year = new Date().getFullYear();
   return (
     <footer className="sm:px-16 py-4 px-8 flex justify-between items-center gap-2 flex-wrap bg-[#161921]">
-      <p className="text-base font-bold text-white">@2023 EpicAnimeVault</p>
+      <p className="text-base font-bold text-white">
+        @{year} by Abdulrahman Mohamed ♥
+      </p>
       <Image
         src="./logo.svg"
         alt="logo"
